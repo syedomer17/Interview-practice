9c2578618a00fb149277d3ecc3688079d8e560f0
syedomer17 <syedomerali2006@gmail.com>
2025-11-27 22:20:38 +0530

    Updated by Omer on 2025-11-27 at 10:20PM

---
+++ arrays/searching-Methods/find.js
@@ -0,0 +1,15 @@
+//Returns the FIRST matching element.
+
+const myarr = [5,7,9,10];
+const findMe = myarr.find(n => n > 6);
+console.log(findMe)
+
+
+// Task:
+
+// Find the first number greater than 100.
+
+const arr = [20,30,21,22,77,88,55,99,101,100]
+
+const findme = arr.find(num => num > 100)
+console.log(findme);
