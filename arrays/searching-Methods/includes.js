44068ad669ab933c9c282c211128b9b80d965152
syedomer17 <syedomerali2006@gmail.com>
2025-11-28 19:17:14 +0530

    Updated by Omer on 2025-11-28 at 07:17PM

---
+++ arrays/searching-Methods/includes.js
@@ -0,0 +1,14 @@
+//Checks if array contains a value.
+const arr  = [1,2,3,4,5];
+
+const has = arr.includes(10);
+console.log(has); // returns only i true or false
+
+// Task:
+
+// Check if 42 exists in ].
+
+const given = [10,20,30,40]
+
+const has42 = given.includes(42);
+console.log(has42);
