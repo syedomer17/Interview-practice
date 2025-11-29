3160dd75818f914963e98cb079bc5a748ece46eb
syedomer17 <syedomerali2006@gmail.com>
2025-11-29 20:10:26 +0530

    Updated by Omer on 2025-11-29 at 08:10PM

---
+++ arrays/searching-Methods/indexOf.js
@@ -0,0 +1,14 @@
+const alpha = ['a', 'b', 'c', 'd', 'e'];
+
+const getc = alpha.indexOf('c');
+console.log(getc)
+
+// Task:
+
+// Find index of "js" inside:
+// ["html", "css", "js", "react"]
+const given = ["html", "css", "js", "react"]
+
+const findJS = given.indexOf("js")
+
+console.log(findJS);
