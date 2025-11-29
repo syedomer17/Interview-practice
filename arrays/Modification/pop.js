8742f6121527085d160be2195c17d2a5201c75b2
syedomer17 <syedomerali2006@gmail.com>
2025-11-29 20:25:15 +0530

    Updated by Omer on 2025-11-29 at 08:25PM

---
+++ arrays/Modification/pop.js
@@ -0,0 +1,18 @@
+//Removes last element.
+
+const array = [1, 2, 3, 4, 5];
+
+array.pop();
+
+console.log(array);
+
+
+// Task:
+
+// Pop the last element from .
+
+const given = [10,20,30]
+
+given.pop()
+
+console.log(given);
---
+++ arrays/Modification/push.js
@@ -0,0 +1,15 @@
+//Add to end.
+
+const arr = [1,2];
+
+arr.push(3);
+console.log(arr)
+
+// Task:
+
+// Push "hello" to: [1,2,3]
+
+const given = [1,2,3]
+
+given.push("hello")
+console.log(given)
