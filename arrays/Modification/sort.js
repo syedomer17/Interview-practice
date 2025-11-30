6b9980a07e5048ad83e8dac491bfed691e981328
syedomer17 <syedomerali2006@gmail.com>
2025-11-30 10:59:24 +0530

    Updated by Omer on 2025-11-30 at 10:59AM

---
+++ arrays/Modification/sort.js
@@ -0,0 +1,15 @@
+const arr = [10, 2, 5];
+
+arr.sort((a,b) => a - b);
+
+console.log(arr)
+
+// Task:
+
+// Sort [100, 5, 20, 1] in descending order.
+
+const given = [100, 5, 20, 1]
+
+given.sort((a,b) => b - a)
+
+console.log(given);
