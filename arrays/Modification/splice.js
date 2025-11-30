ece0f976afce07dd720bc8f8f7e2f6bad3559eae
syedomer17 <syedomerali2006@gmail.com>
2025-11-30 10:56:46 +0530

    Updated by Omer on 2025-11-30 at 10:56AM

---
+++ arrays/Modification/splice.js
@@ -0,0 +1,15 @@
+const arr = [1,2,3,4,5];
+
+arr.splice(1,2) // it will remove all the emements of array from the index 1 to index 2
+console.log(arr)
+
+// Task:
+
+// From [10,20,30,40]
+// Remove 20 and insert 25.
+
+const given = [10,20,30,40]
+
+given.splice(1,1,25) // it will remove 1 element from index 1 and insert 25 at index 1
+
+console.log(given);
