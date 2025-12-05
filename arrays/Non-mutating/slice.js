8612d30dbe426a7cceb0810a0b6542e20397846e
syedomer17 <syedomerali2006@gmail.com>
2025-12-05 15:13:22 +0530

    Updated by Omer on 2025-12-05 at 03:13PM

---
+++ arrays/Non-mutating/slice.js
@@ -0,0 +1,14 @@
+const arr = [1,2,3,4];
+
+const newarr = arr.slice(1,3);
+console.log(newarr)
+console.log(arr)
+// Task:
+
+// Extract [20,30] from [10,20,30,40].
+
+const given = [10,20,30,40]
+
+getit = given.splice(1,3);
+console.log(getit)
+console.log(given)
